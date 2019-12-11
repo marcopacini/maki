5b610303cf755da931439f4eddc9eac0c6dea2dd
Marco Pacini <marco.pacini@adevinta.com>
2019-12-11 12:42:23 +0100

    Create scanner.go

---
+++ compiler/scanner.go
@@ -0,0 +1,378 @@
+package compiler
+
+import "fmt"
+
+type TokenType string
+
+const (
+	And TokenType = "AND"
+	Class = "CLASS"
+	Comma = "COMMA"
+	Dot = "DOT"
+	Else = "ELSE"
+	Eof = "EOF"
+	Equal = "EQUAL"
+	EqualEqual = "EQUAL_EQUAL"
+	False = "FALSE"
+	For = "FOR"
+	Fun = "FUN"
+	Greater = "GREATER"
+	GreaterEqual = "GREATER_EQUAL"
+	Identifier = "IDENTIFIER"
+	If = "IF"
+	LeftBrace = "LEFT_BRACE"
+	LeftParenthesis = "LEFT_PARENTHESIS"
+	LeftSquare = "LEFT_SQUARE"
+	Less = "LESS"
+	LessEqual = "LESS_EQUAL"
+	Let = "LET"
+	Minus = "MINUS"
+	Nil = "NIL"
+	Not = "NOT"
+	NotEqual = "NOT_EQUAL"
+	Number = "NUMBER"
+	Or = "OR"
+	Plus = "PLUS"
+	Print = "PRINT"
+	Return = "RETURN"
+	RightBrace = "RIGHT_BRACE"
+	RightParenthesis = "RIGHT_PARENTHESIS"
+	RightSquare = "RIGHT_SQUARE"
+	Semicolon = "SEMICOLON"
+	Slash = "SLASH"
+	Star = "STAR"
+	String = "STRING"
+	Super = "SUPER"
+	This = "THIS"
+	True = "TRUE"
+	Var = "VAR"
+	While = "WHILE"
+)
+
+var keywords = map[string]TokenType{
+	"and":    And,
+	"class":  Class,
+	"else":   Else,
+	"false":  False,
+	"fun":    Fun,
+	"for":    For,
+	"if":     If,
+	"nil":    Nil,
+	"let":	  Let,
+	"or":     Or,
+	"print":  Print,
+	"return": Return,
+	"super":  Super,
+	"this":   This,
+	"true":   True,
+	"var":    Var,
+	"while":  While,
+}
+
+type Token struct {
+	TokenType
+	Lexeme string
+	Line int
+}
+
+func (t Token) String()	string {
+	return fmt.Sprintf("%v %v %d", t.TokenType, t.Lexeme, t.Line)
+}
+
+type Scanner struct {
+	source []rune
+	start int
+	current int
+	line int
+}
+
+func NewScanner(s string) *Scanner {
+	return &Scanner{
+		source: []rune(s),
+		start: 0,
+		current: 0,
+		line: 1,
+	}
+}
+
+func (s *Scanner) Scan() ([]Token, error) {
+	ts := make([]Token, 0)
+
+	for ;; {
+		t, err := s.scanToken()
+		if err != nil {
+			return nil, err
+		}
+
+		switch t.TokenType {
+		case Eof:
+			return append(ts, *t), nil
+		default:
+			ts = append(ts, *t)
+		}
+	}
+}
+
+func (s *Scanner) scanToken() (*Token, error) {
+	s.start = s.current
+	s.trim()
+
+	if s.isEnd() {
+		eof := &Token{
+			TokenType: Eof,
+			Lexeme:    "",
+			Line:      s.line,
+		}
+		return eof, nil
+	}
+
+	r := s.advance()
+	switch r {
+	case '(':
+		{
+			return s.makeToken(LeftParenthesis), nil
+		}
+	case ')':
+		{
+			return s.makeToken(RightParenthesis), nil
+		}
+	case '{':
+		{
+			return s.makeToken(LeftBrace), nil
+		}
+	case '}':
+		{
+			return s.makeToken(RightBrace), nil
+		}
+	case '[':
+		{
+			return s.makeToken(LeftSquare), nil
+		}
+	case ']':
+		{
+			return s.makeToken(RightSquare), nil
+		}
+	case ';':
+		{
+			return s.makeToken(Semicolon), nil
+		}
+	case ',':
+		{
+			return s.makeToken(Comma), nil
+		}
+	case '.':
+		{
+			return s.makeToken(Dot), nil
+		}
+	case '+':
+		{
+			return s.makeToken(Plus), nil
+		}
+	case '-':
+		{
+			return s.makeToken(Minus), nil
+		}
+	case '*':
+		{
+			return s.makeToken(Star), nil
+		}
+	// Multi-character lexeme
+	case '!':
+		{
+			if s.isNext('=') {
+				return s.makeToken(NotEqual), nil
+			}
+			return s.makeToken(Not), nil
+		}
+	case '=':
+		{
+			if s.isNext('=') {
+				return s.makeToken(EqualEqual), nil
+			}
+			return s.makeToken(Equal), nil
+		}
+	case '>':
+		{
+			if s.isNext('=') {
+				return s.makeToken(GreaterEqual), nil
+			}
+			return s.makeToken(Greater), nil
+		}
+	case '<':
+		{
+			if s.isNext('=') {
+				return s.makeToken(LessEqual), nil
+			}
+			return s.makeToken(Less), nil
+		}
+	case '/':
+		{
+			// Single-line comment
+			if s.isNext('/') {
+				for s.peek() != '\n' && !s.isEnd() {
+					_ = s.advance()
+				}
+				return s.scanToken()
+			}
+
+			// Multi-line comment
+			if s.isNext('*') {
+				for ;; {
+					if s.isEnd() {
+						return nil, fmt.Errorf("comment not terminated at line %d", s.line)
+					}
+
+					r = s.advance()
+					switch r {
+					case '\n':
+						{
+							s.line++
+							break
+						}
+					case '*':
+						{
+							if s.peek() == '/' {
+								_ = s.advance()
+								return s.scanToken()
+							}
+						}
+					}
+				}
+			}
+
+			return s.makeToken(Slash), nil
+		}
+	case '"':
+		{
+			for s.peek() != '"' && !s.isEnd() {
+				if s.peek() == '\n' {
+					s.line++
+				}
+				_ = s.advance()
+			}
+
+			if s.isEnd() {
+				return nil, fmt.Errorf("unterminated string at line %d", s.line)
+			}
+			_ = s.advance()
+
+			t := &Token{
+				TokenType: String,
+				Lexeme: string(s.source[s.start:s.current]),
+				Line: s.line,
+			}
+			return t, nil
+		}
+	default:
+		{
+			if isDigit(r) {
+				for isDigit(s.peek()) {
+					_ = s.advance()
+				}
+
+				if s.peek() == '.' && isDigit(s.peekNext()) {
+					s.advance()
+
+					for isDigit(s.peek()) {
+						s.advance()
+					}
+				}
+
+				return s.makeToken(Number), nil
+			}
+
+			if isLetter(r) {
+				for isLetter(s.peek()) || isDigit(s.peek()) {
+					s.advance()
+				}
+
+				if t, ok := keywords[string(s.source[s.start:s.current])]; ok {
+					return s.makeToken(t), nil
+				}
+
+				return s.makeToken(Identifier), nil
+			}
+
+			return nil, fmt.Errorf("unkown character '%v' at line %d", string(r), s.line)
+		}
+
+	}
+}
+
+func (s *Scanner) isEnd() bool {
+	return s.current >= len(s.source)
+}
+
+func (s *Scanner) peek() rune {
+	if s.isEnd() {
+		return '\x00'
+	}
+	return s.source[s.current]
+}
+
+func (s *Scanner) peekNext() rune {
+	if s.current+1 >= len(s.source) {
+		return '\x00'
+	}
+	return s.source[s.current-1]
+}
+
+func (s *Scanner) advance() rune {
+	s.current++
+	return s.source[s.current-1]
+}
+
+func (s *Scanner) isNext(r rune) bool {
+	if s.isEnd() || s.source[s.current] != r {
+		return false
+	}
+
+	s.current++
+	return true
+}
+
+func (s *Scanner) trim() {
+	for !s.isEnd() {
+		r := s.peek()
+		switch r {
+		case ' ', '\t', '\r':
+			{
+				s.start++
+				s.current++
+				break
+			}
+		case '\n':
+			{
+				s.start++
+				s.current++
+				s.line++
+				break
+			}
+		default:
+			return
+		}
+	}
+}
+
+func isDigit(r rune) bool {
+	if r >= '0' && r <= '9' {
+		return true
+	}
+	return false
+}
+
+func isLetter(r rune) bool {
+	if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') {
+		return true
+	}
+
+	return false
+}
+
+func (s *Scanner) makeToken(t TokenType) *Token {
+	return &Token{
+		TokenType: t,
+		Lexeme:    string(s.source[s.start:s.current]),
+		Line:      s.line,
+	}
+}
---
+++ compiler/scanner_test.go
@@ -0,0 +1,97 @@
+package compiler
+
+import "testing"
+
+func listTokenTypes(tokens []Token) []TokenType {
+	tt := make([]TokenType, len(tokens))
+	for i, t := range tokens {
+		tt[i] = t.TokenType
+	}
+	return tt
+}
+
+func TestScanner_Scan(t *testing.T) {
+	tcs := []struct {
+		name string
+		in  string
+		out []TokenType
+	}{
+		{
+			name: "Parenthesis Tokens",
+			in: "(){}[]",
+			out: []TokenType{ LeftParenthesis, RightParenthesis, LeftBrace, RightBrace, LeftSquare, RightSquare, Eof },
+		},
+		{
+			name: "Single Character Tokens",
+			in: "+ - * / , ; ! > <",
+			out: []TokenType{ Plus, Minus, Star, Slash, Comma, Semicolon, Not, Greater, Less, Eof },
+		},
+		{
+			name: "Multi Characters Tokens",
+			in: "== != >= <=",
+			out: []TokenType{ EqualEqual, NotEqual, GreaterEqual, LessEqual, Eof },
+		},
+		{
+			name: "Single-line Comment Token",
+			in: "// This text have to be ignored",
+			out: []TokenType{ Eof },
+		},
+		{
+			name: "Multi-line comment token",
+			in: "/* This text have to be ignored */",
+			out: []TokenType{ Eof },
+		},
+		{
+			name: "String Token",
+			in: "\"This is a string!\"",
+			out: []TokenType{ String, Eof },
+		},
+		{
+			name: "Number Token",
+			in: "1 12 12.3",
+			out: []TokenType{ Number, Number, Number, Eof },
+		},
+		{
+			name: "Logic Keywords Tokens",
+			in: "and or true false",
+			out: []TokenType{ And, Or, True, False, Eof },
+		},
+		{
+			name: "Conditional Keywords Tokens",
+			in: "if else for while",
+			out: []TokenType{ If, Else, For, While, Eof },
+		},
+		{
+			name: "Function Keywords Tokens",
+			in: "fun return",
+			out: []TokenType{ Fun, Return, Eof },
+		},
+		{
+			name: "Type Keywords Tokens",
+			in: "class var let nil",
+			out: []TokenType{ Class, Var, Let, Nil, Eof },
+		},
+		{
+			name: "Print and Identifier Tokens",
+			in: "print x",
+			out: []TokenType{ Print, Identifier, Eof },
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			scanner := NewScanner(tc.in)
+			tokens, _ := scanner.Scan()
+
+			if len(tokens) != len(tc.out) {
+				t.Errorf("got %d tokens, want %d: %v != %v", len(tokens), len(tc.out), tc.out, listTokenTypes(tokens))
+			} else {
+				for i := range tokens {
+					if tokens[i].TokenType != tc.out[i] {
+						t.Errorf("got %v, want %v: %v != %v", tokens[i].TokenType, tc.out[i], tc.out, listTokenTypes(tokens))
+					}
+				}
+			}
+		})
+	}
+}
